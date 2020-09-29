428d19174e5b76d50787607e7286375b6dfe4961
waseber <yvesgerber@gmail.com>
2020-09-29 11:40:55 -0400

    adds foundational items for css/sass, fonts, gulp. Also including reference files

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', gulp.series(function() {
+	return gulp.src(['./src/css/sass/**/*.scss', '!./src/css/sass/helpers/*.scss'])
+		.pipe(sass())
+		.pipe(gulp.dest('./src/css'));
+}));
+
+
+gulp.task('watch', gulp.series(function() {
+    gulp.watch('./src/css/sass/**/*.scss', gulp.series('sass'));
+}));
+
+gulp.task('default', gulp.series('watch'));
