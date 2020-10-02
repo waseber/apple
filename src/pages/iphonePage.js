6697a566f228aa024bf19704f4ab0aaeb4dd2aa9
waseber <yvesgerber@gmail.com>
2020-10-02 15:33:57 -0400

    iphone slider stuff

--- src/pages/iphonePage.js
+++ src/pages/iphonePage.js
@@ -5,17 +5,32 @@ import logo from '../apple_logo.svg';
 
 
 const IphonePage = () => {
-    const [color, setColor] = useState("black");
+    const [sliderVal, setSliderVal] = useState(0);
     const GetSliderVal = () =>{
         const slider = document.getElementById('thumb-slider');
+        setSliderVal(slider.value);
         //console.log(slider.value)
+        FadeThumbs();
+        SlideImages();
     }
+    const FadeThumbs = () =>{
+        const front = document.getElementById('iphone-thumb-front');
+        const back = document.getElementById('iphone-thumb-back');
+        back.style.opacity = sliderVal/100 + 0.5;
+        front.style.opacity = 1 - (sliderVal/100);
+   }
+   const SlideImages = () => {
+        const front = document.getElementById('iphone-front');
+        const back = document.getElementById('iphone-back');
+        back.style.opacity = sliderVal/100 *2;
+        front.style.opacity = 1 - (sliderVal/100 *2);
+   }
     return(
         <div className="page display-flex flex-col justify-center align-center">
             <NavBar landing="false"/>
             <div className="main-container display-flex">
                 <main id="iphone-page-content" className="page-content-left-margin ">
-                    <section id="iphone-blurb" className="background-gradient-grey w-50">
+                    <section id="iphone-blurb" className="background-gradient-grey">
                     <img src={logo} height="200" width="200" className="abs-centered"  />
                     <h1 className="red">iPhone</h1>
                     <h2>The ultimate<br/>iPhone</h2>
@@ -25,20 +40,25 @@ const IphonePage = () => {
                         <h4 className="muted">From $699</h4>
                         <a href="#" className="buy-now-red">Buy Now ></a>
                     </div>
-                    <div className="display-flex flex-col align-center" style={{gridArea: 'thumbs', paddingTop: '5vh'}}>
+
+                    <div className="thumbs-container display-flex flex-col align-center">
                         <div>
                             <img id="iphone-thumb-front" className="iphone-thumbs" src={process.env.PUBLIC_URL + '/Images/apple-iphonexs-max-gold.png'} />
-                            <img id="iphone-thumb-back" className="iphone-thumbs" src={process.env.PUBLIC_URL + '/Images/apple-iphonexs-max-gold-back-3.png'} />
+                            <img id="iphone-thumb-back" className="iphone-thumbs" src={process.env.PUBLIC_URL + '/Images/apple-iphonexs-max-gold-back-3.png'} style={{opacity: "0.5"}}/>
                         </div>
                             
                         <input 
                             type="range"
                             id="thumb-slider"
                             min="0" 
-                            max="100"
+                            max="50"
                             defaultValue="0"
                             onInput={()=>GetSliderVal()}
                         />
+                    </div>
+                    <div className="image-container display-flex" >
+                        <img id="iphone-front" className="abs-centered iphone-images" src={process.env.PUBLIC_URL + '/Images/apple-iphonexs-max-gold@2x.png'} />
+                        <img id="iphone-back" className="abs-centered iphone-images" src={process.env.PUBLIC_URL + '/Images/apple-iphonexs-max-gold-back-3@2x.png'} style={{opacity: "0"}}/>
                     </div>
 
                    
